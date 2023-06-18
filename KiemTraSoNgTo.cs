0f43ac2b16b1f9c5927566357065aabf3bb9fec3
Lê Quốc Thước <102453854+thuocle@users.noreply.github.com>
2023-06-18 21:48:50 +0700

    //Ham kiem tra so nguyen to

---
+++ KiemTraSoNgTo.cs
@@ -0,0 +1,15 @@
+//Ham kiem tra so nguyen to
+        public static bool isPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        }
