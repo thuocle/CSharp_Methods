a547c7e9228aaecf8c3c736d0a0b9578b504a1f6
Lê Quốc Thước <102453854+thuocle@users.noreply.github.com>
2023-06-20 13:35:21 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace CSharpMethods
 
     class Program
     {
-        //Ham nhap mang
+        //Ham nhap mang/
         public static void GetArr(int[] arr, int length)
         {
             for (int i = 0; i < length; i++)
