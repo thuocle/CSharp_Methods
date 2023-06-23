a17913ba1ed52281ba6a7aa67adf762304cd5ef4
Lê Quốc Thước <102453854+thuocle@users.noreply.github.com>
2023-06-23 19:23:23 +0700

    //CS02-1-11: Thuật toán sắp xếp nổi bọt tang dan

---
+++ bubbleSort.cs
@@ -0,0 +1,16 @@
+//CS02-1-11: Thuật toán sắp xếp nổi bọt tang dan
+        public static void bubbleSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        int tmp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = tmp;
+                    }
+                }
+            }
+        }
